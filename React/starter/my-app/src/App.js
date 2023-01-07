3b3244f85c23d54186385617f7a9713603ea31cb
Radicitus <novivolatus@gmail.com>
2023-01-06 21:47:22 -0800

    State

--- React/starter/my-app/src/App.js
+++ React/starter/my-app/src/App.js
@@ -2,22 +2,32 @@ import React, { useState } from "react";
 import "./style.css";
 
 function App() {
-  const [value, setValue] = useState("");
+  const [count, setCount] = useState(0);
+  const [prevCount, setPrevCount] = useState(0);
 
-  const eventHandler = (e) => {
-    setValue(e.target.value);
-    console.log(e.target);
+  // const [state, setState] = useState({ count: 0, user: 'Bob' });
+
+  const handleClick = () => {
+    setCount((prev) => {
+      setPrevCount(prev);
+    });
+    setCount(count + 1);
   };
+
+  // const handleClick = () => {
+  //   setState({
+  //     ...state,
+  //     count: state.count + 1,
+  //   });
+  // };
 
   return (
     <>
-      <input
-        value={value}
-        placeholder={"Enter some text"}
-        onChange={eventHandler}
-      />
-
-      <p>{value}</p>
+      <h3>Current count: {count}</h3>
+      <h3>Previous count: {prevCount}</h3>
+      {/* <h3>Count: {state.count}</h3>
+      <h3>User: {state.user}</h3> */}
+      <button onClick={handleClick}>Increment</button>
     </>
   );
 }
