220f7b1588a83523a263488732091383b90ebac2
Radicitus <novivolatus@gmail.com>
2023-01-06 21:58:18 -0800

    Lifecycle and Effects

--- React/starter/my-app/src/App.js
+++ React/starter/my-app/src/App.js
@@ -1,34 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [prevCount, setPrevCount] = useState(0);
+const Countdown = ({ hr, min, sec }) => {
+  const [over, setOver] = useState(false);
+  const [paused, setPaused] = useState(true);
+  const [[h, m, s], setTime] = useState([hr, min, sec]);
 
-  // const [state, setState] = useState({ count: 0, user: 'Bob' });
-
-  const handleClick = () => {
-    setCount((prev) => {
-      setPrevCount(prev);
-    });
-    setCount(count + 1);
+  const tick = () => {
+    if (paused || over) {
+      return;
+    }
+    if (h === 0 && m === 0 && s === 0) {
+      setOver(true);
+    } else if (m === 0 && s === 0) {
+      setTime([h - 1, 59, 59]);
+    } else if (s === 0) {
+      setTime([h, m - 1, 59]);
+    } else {
+      setTime([h, m, s - 1]);
+    }
   };
 
-  // const handleClick = () => {
-  //   setState({
-  //     ...state,
-  //     count: state.count + 1,
-  //   });
-  // };
+  const handleReset = () => {
+    setTime([hr, min, sec]);
+    setPaused(true);
+    setOver(false);
+  };
+
+  const handlePause = () => setPaused(!paused);
+
+  const fmt = (val) => val.toString().padStart(2, "0");
+
+  useEffect(() => {
+    let ticker = setInterval(() => tick(), 1000);
+    return () => {
+      clearInterval(ticker);
+    };
+  });
 
   return (
     <>
-      <h3>Current count: {count}</h3>
-      <h3>Previous count: {prevCount}</h3>
-      {/* <h3>Count: {state.count}</h3>
-      <h3>User: {state.user}</h3> */}
-      <button onClick={handleClick}>Increment</button>
+      <h3 className="countdown">{`${fmt(h)}:${fmt(m)}:${fmt(s)}`}</h3>
+      <button onClick={handlePause}>{paused ? "Start" : "Pause"}</button>
+      <button onClick={handleReset}>Reset</button>
     </>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Countdown hr={0} min={0} sec={10} />
+    </div>
   );
 }
 
